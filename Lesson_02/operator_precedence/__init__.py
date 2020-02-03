7d4257fefd06d98dfc0190612523cd7657922bdf
Adam <a.beardsley79@gmail.com>
2020-02-03 18:23:16 +0000

    update

--- Lesson_02/operator_precedence/__init__.py
+++ Lesson_02/operator_precedence/__init__.py
@@ -4,10 +4,17 @@ b = 3
 print(a + b / 3 - 4 * 12)
 
 #operator order
+
+# division and multiplication come before addition and subtraction
+
+
 # * / + -
 
 #first: 12 * 4 = 48    second: b / 3.0 = 1 third: a + 1 = 13 forth: 13 - 48 = -35.0
 print(a + (b / 3) - (4 * 12))
 
 
-print(16 / 5 - 3 * 12 + 7)
+
+
+
+

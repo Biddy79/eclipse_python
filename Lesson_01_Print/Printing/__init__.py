b73bac8069491abbeda743522ba29c38040ad9fb
Adam <a.beardsley79@gmail.com>
2020-02-03 13:08:33 +0000

    Divison operator work done in project Lesson_01_Print

--- Lesson_01_Print/Printing/__init__.py
+++ Lesson_01_Print/Printing/__init__.py
@@ -1,5 +1,44 @@
 print("My hovercarft is full of eels")
 
+print()
+
 print(6*7)
 
+print()
+
+#using escape tab
 print("Number1 \t The Larch \nNumber2 \t The Horse Chestnut")
+
+print()
+
+name = "Bob"
+age = 25
+
+print("may name is " + name)
+
+#this will show error cannot concatenate sting with int
+#print("may age is " + age)
+
+age_in_words = "25"
+print("My age is " + age_in_words) 
+
+
+print()
+
+#division operator
+a = 12
+b = 3
+
+print(a / b)  # note this will do floating point division  
+print(a // b) # THis will do integer division
+
+print()
+
+#note must use // here to do integer division or error will be given
+for i in range(1, a // b):
+    print(i)
+
+
+
+
+

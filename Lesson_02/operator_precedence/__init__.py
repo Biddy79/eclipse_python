35a61ccdeed38564988806c754e94e3fed40ba7b
Adam <a.beardsley79@gmail.com>
2020-02-03 18:21:28 +0000

    added new project Lesions_02 operator precedence

---
+++ Lesson_02/operator_precedence/__init__.py
@@ -0,0 +1,13 @@
+a = 12
+b = 3
+
+print(a + b / 3 - 4 * 12)
+
+#operator order
+# * / + -
+
+#first: 12 * 4 = 48    second: b / 3.0 = 1 third: a + 1 = 13 forth: 13 - 48 = -35.0
+print(a + (b / 3) - (4 * 12))
+
+
+print(16 / 5 - 3 * 12 + 7)

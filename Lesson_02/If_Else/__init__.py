be5b84667ac0210390ccad19ab1e7071e02093d0
Adam <a.beardsley79@gmail.com>
2020-02-06 20:20:37 +0000

    added elif to Lesson_02 If_Else project

--- Lesson_02/If_Else/__init__.py
+++ Lesson_02/If_Else/__init__.py
@@ -1,8 +1,18 @@
 name = input("Please enter you Name: ")
 age = int(input("Hello {0} Please enter your age: ".format(name)))
-
+ 
 if age <= 18:
     print("sorry {0} You are not old enough to vote Please come back in {1} Years".format(name, 18 - age))
 else:
-    print("Hi {0} you are old enough to vote. ".format(name))
+    print(f"Hi {name} you are old enough to vote. ")
 
+
+pin = int(input("Please enter your pin"))
+
+if pin == 959:
+    print(f"you entered {pin}. pin correct !!!")
+elif pin > 959:
+    print(f"you entered {pin}. pin is a lower number!!!")
+else:
+    print(f"you entered {pin}. pin is a higher number !!! ")
+    

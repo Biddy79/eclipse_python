1ce0c95f252b0ca5dc8f682b9c6961a0ddf0b953
Adam <a.beardsley79@gmail.com>
2020-02-08 15:19:55 +0000

    more added to If_Else in Lession _02 project

--- Lesson_02/If_Else/__init__.py
+++ Lesson_02/If_Else/__init__.py
@@ -1,18 +1,52 @@
 name = input("Please enter you Name: ")
 age = int(input("Hello {0} Please enter your age: ".format(name)))
- 
+  
 if age <= 18:
     print("sorry {0} You are not old enough to vote Please come back in {1} Years".format(name, 18 - age))
 else:
     print(f"Hi {name} you are old enough to vote. ")
+ 
+ 
+# pin = 959
+# guess = int(input("Please enter your pin"))
+#   
+#  
+# if guess > pin:
+#     print(f"you entered {guess}. pin is a lower number!!!")
+#     guess = int(input("please enter pin"))
+#     if guess == pin:
+#         print("pin correct")
+#     else: print("Sorry pin is incorrect")
+# elif guess < 959:
+#     print(f"you entered {guess}. pin is a higher number !!! ")
+#     guess = int(input("please enter pin"))
+#     if guess == pin:
+#         print("pin correct")
+#     else: print("Sorry pin is incorrect")
+# else:
+#     print("pin correct !!!")
 
+#better way!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-pin = int(input("Please enter your pin"))
+pin = 959
+guess = int(input("Please enter your pin: "))
 
-if pin == 959:
-    print(f"you entered {pin}. pin correct !!!")
-elif pin > 959:
-    print(f"you entered {pin}. pin is a lower number!!!")
+if guess == pin:
+    print("pin correct")
 else:
-    print(f"you entered {pin}. pin is a higher number !!! ")
+    if guess < pin:
+        print("pin is a higher number")
+    else:
+        print("pin is a lower number")
+    guess = int(input("please enter pin again"))
+    if guess == pin:
+        print("pin correct")
+    else:
+        print("sorry pin still incorrect!!!")
+
+    
+        
+
+    
+    
     

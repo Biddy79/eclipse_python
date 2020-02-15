1986118f583fda7674a10dfa47ead82f24b70bb4
Adam <a.beardsley79@gmail.com>
2020-02-15 14:18:57 +0000

    project AdvancedPortScanner_0/Adding_parser added commScan function

--- AdvancedPortScanner_0/Adding_parser/__init__.py
+++ AdvancedPortScanner_0/Adding_parser/__init__.py
@@ -1,26 +1,21 @@
-#!/usr/bin/python
-# from _socket import *
-# import optparse
-# from threading import *
-# 
-# def main():
-#     parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -P <taget port>')
-#     parser.add_option('-H,', dest='tgtHost', type='string', help='specify target host')
-#     parser.add_option('-P,', dest='tgtPort', type='string', help='specify target ports separated by comma')
-#     (options, args) = parser.parse_args()
-#     tgtHost = options.tgtHost
-#     tgtPorts = str(options.tgtPort).split(',')
-#     if (tgtHost == None) | (tgtPorts[0] == None):
-#         print(parser.usage)
-#         exit(0)
-    #portScan(tgtHost,tgtPorts)
-    
-#main()
 
 import argparse
-from _socket import gethostbyname, gethostbyaddr
+from _socket import * 
+from threading import * 
 
-def portScan(tgtHost, tgtPort):
+def connScan(tgtHost, tgtPort):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print(f"[+] {tgtPort}/tcp Open")
+    except:
+        print(f"[-] {tgtHost}/tcp Closed")
+    finally:
+        sock.close()
+        
+    
+
+def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
@@ -30,30 +25,35 @@ def portScan(tgtHost, tgtPort):
         print("[+] Scan results for:" + tgtName[0])
     except:
         print("[+] Scan results for: " + tgtIP)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        t = Thread(target = connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+    
     
 def main():
-    parser = argparse.ArgumentParser(description="Usage of program: -tgtHost <target host> -tgtPost <taget port>")
+    parser = argparse.ArgumentParser(description="Usage of program: -H <target host> -p <taget port>")
 
     #parser add argument   #name    #help description             #variable type
-    parser.add_argument('-tgtHost', help = 'specify target host', type = str)
-    parser.add_argument('-tgtPort', help = 'specify target ports separated by comma', type = str)
+    parser.add_argument('-H', help = 'specify target host', type = str)
+    parser.add_argument('-p', help = 'specify target ports separated by comma', type = str)
     
     #must set args = pareser argumets befor creating tgtHost and tgtPort = toarguments in parser
     args = parser.parse_args()
     
     #setting variables to argument
-    tgtHost = args.tgtHost
-    tgtPort = str(args.tgtPort).split(',')
+    tgtHost = args.H
+    tgtPorts = str(args.p).split(',')
     
     #checking if tgtHost or tgtPort are == to empty string if so print description
-    if tgtHost == None or tgtPort[0] == None:
+    if tgtHost == None or tgtPorts[0] == None:
         print(parser.description)
         exit(0)
     else:
-        print(f"Host Entered: {tgtHost}  Port Entered: {tgtPort}")
+        print(f"Host Entered: {tgtHost}  Port Entered: {tgtPorts}")
         
-    #port scanner to be created!!!
-    #portScan(tgtHost,tgtPorts)
+    #portScan function called
+    portScan(tgtHost,tgtPorts)
 
 if __name__ == '__main__':
     main()

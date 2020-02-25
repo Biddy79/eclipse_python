2e00de82ac08465d9b9ef9d7bf4f113f2ada51d6
Adam Beardsley <a.beardsley79@gmail.com>
2020-02-25 13:50:10 +0000

    More_on_list added to Lists_Ranges_and_Tuples

---
+++ Lists_Ranges_and_Tuples/More_on_lists/__init__.py
@@ -0,0 +1,55 @@
+from audioop import reverse
+list1 = []
+#creating a list using list constructor
+list2 = list()
+print(list1)
+print(list2)
+
+#prints each character separately
+print(list("The list are equal"))
+
+odd = [1,3,5,7,9,]
+even = list([2,4,6,8,])
+
+#list of lists
+numbers = [odd, even]
+print(numbers)
+
+#seting a variabel called new numbers the point to the same values as numbers
+new_numbers = numbers
+#list in new_numbrs point to the list in numbers
+if new_numbers == numbers:
+    print("true")
+else:
+    print("false")
+#new_nubers is pointint to the same list therefor is the same     
+if new_numbers is numbers:
+    print("true")
+else:
+    print("false")
+#using list constructor to creat a new list passing numbers into the constructor
+new_new_numbers = list(numbers)
+
+print("-" * 20)
+
+#new_new_nubers is its own copy of numbers therfore new_new_numbers is not numbers
+if new_new_numbers is numbers:
+    print("true")
+else:
+    print("false")
+#But!! new_new_nubers is equal to the values in the numbers list as it point 
+#to the same list
+if new_new_numbers == numbers:
+    print("true")
+else:
+    print("false")
+
+#revrsing the order of new_numbers
+new_numbers.sort(reverse=True)
+print(new_numbers)
+#NOTE this also reverser the order of numbers as they point to the same list
+print(numbers)
+
+#new_new_nubers remains in order as it is its own list
+#created using the constructor List()
+print(new_new_numbers)

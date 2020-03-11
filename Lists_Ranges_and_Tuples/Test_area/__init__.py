c18546144627c3a5553fe222e167985b8e4f9ef8
Adam <a.beardsley79@gmail.com>
2020-03-11 20:17:44 +0000

    accessing list in tuple in different ways

--- Lists_Ranges_and_Tuples/Test_area/__init__.py
+++ Lists_Ranges_and_Tuples/Test_area/__init__.py
@@ -1,16 +1,14 @@
 sing_in_record = "D.K ", 21, ([])
 #unpacking the tuple
-initials, age, time_in_out = sing_in_record
+company, location, time_in_out = sing_in_record
     
 print(sing_in_record)
 
-#these values cannot be changed as the are tuples
-initials = input(print("Enter first and last initials: "))
-age = input(print("Enter age: "))
+#company, location these values cannot be changed as the are tuples
 
 #these values can be changed as they are list inside of tuple
-time_in = input(print("Enter Time in: "))
-time_out = input(print("Enter Time out: "))
+time_in = float(input(print("Enter Time in: ")))
+time_out = float(input(print("Enter Time out: ")))
 
 #below are two different ways of setting values in a list within a tuple
 sing_in_record[2].append((time_in))
@@ -20,6 +18,13 @@ time_in_out.append((time_out))
 #iterating of items in tuple 
 for info in sing_in_record:
     print(f"{info}", end = "")
+
+print("\n")
+
+#can also access list in tuple like this
+print(sing_in_record[2][0])
+#or like this
+print(time_in_out[1])
     
 
 

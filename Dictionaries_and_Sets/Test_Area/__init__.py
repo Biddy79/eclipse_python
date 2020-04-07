01749ca5457f5a115d1d6bded3f80e5c3003cb9f
Adam <a.beardsley79@gmail.com>
2020-04-07 12:18:21 +0100

    converting tupel to set in test _area

--- Dictionaries_and_Sets/Test_Area/__init__.py
+++ Dictionaries_and_Sets/Test_Area/__init__.py
@@ -1,17 +1,38 @@
 user_input = input("Enter some text here: ").casefold()
-
+ 
 user_serch = input("Enter letter you are searching for: ").casefold()
-
+ 
 #each letter in user_input separated into individual characters
 characters = list(user_input) 
-
+ 
 print(characters)
-
+ 
 letter_search = "";
-
+ 
 for i in characters:
     if user_serch == i:
         letter_search += i
-
+ 
 print(f"Here are all the letters you where searching for {letter_search.split()} ")
-    
+
+print("-" * 20)  
+     
+tuple
+transport_0 = "car", "plain", "boat"
+transport_1 = "train", "car", "plain", "sub"
+
+print(transport_0)
+print(transport_1)
+
+#unpacking the tupel
+BMW, Boeing757, Cruies_Liner = transport_0 
+print(f"Here are the types of transport in more detail "
+    "car:{BMW}, plain:{Boeing757}, Boat:{Cruies_Liner}")
+
+transport_0_set = set(transport_0)
+
+#using difference function to show difference between sets
+#note transport_1 is still a tuple but can be passed as a parameter
+print(transport_0_set.difference(transport_1))
+
+

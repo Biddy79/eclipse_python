df55c95bd7c7e3a4dc24a549b1b4ac98072057cf
Adam Beardsley <a.beardsley79@gmail.com>
2020-05-11 20:41:38 +0100

    more work done on time_game

--- Modules_and_functions/Time_game/__init__.py
+++ Modules_and_functions/Time_game/__init__.py
@@ -1,14 +1,25 @@
 import time
+import random
+
+
+
+input("press enter to start")
+
+wait_time = random.randint(1, 7)
+
+time.sleep(wait_time)
 
 start_time = time.time()
 
-input("press enter....")
+input("Press enter to stop")
 
 end_time = time.time()
 
-print("epoch time at start of program {0}".format(start_time))
+print("Time at start of program " + time.strftime("%X", time.localtime(start_time)))
 
-print("epoch time at end of program {0:20}".format(end_time))
+print("Time at end of program " + time.strftime("%X", time.localtime(end_time)))
+
+print(f"your reaction time in seconds:  {end_time - start_time}")
 
 
 

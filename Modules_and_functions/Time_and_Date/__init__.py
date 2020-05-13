e3e1e0b0895faa87115b86b8f5c6f2634412a8fc
Adam <a.beardsley79@gmail.com>
2020-05-13 21:14:30 +0100

    changed timer to perf_counter in time_game added more functions to Time_and_Date package

--- Modules_and_functions/Time_and_Date/__init__.py
+++ Modules_and_functions/Time_and_Date/__init__.py
@@ -1,7 +1,12 @@
 import time
 
-#time() number of secons from epoch
+#############Time functions to return real dates and times also epoch#################### 
+
+#time() number of seconds from epoch
 print(time.time())
+
+#gives information on time() functions
+print(time.get_clock_info('time'))
 
 #localtime() returns tuple with local time year date in GMT
 print(time.localtime())
@@ -9,14 +14,32 @@ print(time.localtime())
 #gettime() returns tuple with local time year date in UTC  
 print(time.gmtime())
 
-#gettime(0) returns tuple with year date time of tart of epoch
+#gettime(0) returns tuple with year, date, and time of start of epoch
 print(time.gmtime(0))
 
-#naming time method to acsess tuple data
+#naming time method to access tuple data
 time_here = time.localtime()
 
-#acsessing year in tupel returned from localtime() using array 
+#accessing year in tuple returned from localtime() using array 
 print(f"year: {time_here[0]}")
 
 #acsessing year in tupel returned from localtime() using name
 print(f"year: {time_here.tm_year}")
+
+print("-" * 30)
+
+########## More functions to measure elapsed time ###########################
+#monotonic means clock dose not count backwards. perf_counter and monotonic functions both are monotonic
+
+print(time.get_clock_info('monotonic'))
+print(time.monotonic())
+
+#perf_counter very precise timer good for timing code. better to uses this than monotonic
+print(time.get_clock_info('perf_counter'))
+print(time.perf_counter()) 
+
+#times how long cpu takes to run a process rather than elapsed time
+print(time.get_clock_info('process_time'))
+print(time.process_time())
+
+#see Python documentation on time
--- Modules_and_functions/Time_game/__init__.py
+++ Modules_and_functions/Time_game/__init__.py
@@ -9,11 +9,11 @@ wait_time = random.randint(1, 7)
 
 time.sleep(wait_time)
 
-start_time = time.time()
+start_time = time.perf_counter()
 
 input("Press enter to stop")
 
-end_time = time.time()
+end_time = time.perf_counter()
 
 print("Time at start of program " + time.strftime("%X", time.localtime(start_time)))
 

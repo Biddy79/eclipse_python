e34e53d91282d1ffb34adfb814792bfe99f38ee3
Adam <a.beardsley79@gmail.com>
2020-05-22 14:56:20 +0100

    started working with pytz timezones

--- Modules_and_functions/Timezones_pytz/__init__.py
+++ Modules_and_functions/Timezones_pytz/__init__.py
@@ -1,3 +1,10 @@
 import pytz
+import datetime
 
+country = 'Europe/Moscow'
 
+tz_to_display = pytz.timezone(country)
+local_time = datetime.datetime.now(tz=tz_to_display)
+
+print(f"The time in {country} is {local_time}")
+print(f"UTC is {datetime.datetime.utcnow()}")

8f35d20044befb69fb75dae2d504078417703b46
Adam <a.beardsley79@gmail.com>
2020-05-30 14:55:30 +0100

    time_challenge: added awear utc time

--- Modules_and_functions/Time_challenge/__init__.py
+++ Modules_and_functions/Time_challenge/__init__.py
@@ -26,7 +26,6 @@ countrys = {"1": 'Mexico/BajaNorte',
             "9": 'Canada/Central'}
 
 for x in countrys:
-    
     print(f"{x}: {countrys[x]}")
     
 
@@ -38,9 +37,18 @@ while True:
     elif user_input not in dict.keys(countrys):
         print("you have made an invalid entry")
     else:
+        #store country's time zone object in dz_to_display
         dz_to_display = pytz.timezone(countrys[user_input])
+        #store date and time in local_time setting tz object in parameter to country
         local_time = datetime.datetime.now(tz=dz_to_display)
+        #store UTC local time in utc_awear. making time zone aware using .astimezone() function
+        utc_aware_time = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(dz_to_display)
+        #printing info to user
         print(f"local date and time in {countrys[user_input]} is {local_time}")
+        print(f"UTC date and time in {countrys[user_input]} is {utc_aware_time}")
+        
+       
+        
     
     
 

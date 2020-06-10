9d897268e93910b83488569c6a549a00d01b3f3f
Adam <a.beardsley79@gmail.com>
2020-06-10 21:48:12 +0100

    competed Grid_and_geometry by adding mainWindow columnconfigure(), adding a border in left and right frames, and column configureing right frame to then expanding button2

--- Tkinter/Grid_and_geometry/__init__.py
+++ Tkinter/Grid_and_geometry/__init__.py
@@ -2,9 +2,13 @@ try:
     import tkinter
 except ImportError:
     import Tkinter as tkinter
-    
+
+#Note!    
 #.grid() uses row and column to set grid lay out over mainWindow
 #.grid() also uses stick. this sets position using, north, south, east, and west ('n','s','e','w')
+
+#careful not to confuse columnconfigure() with config()  
+
     
 mainWindow = tkinter.Tk()
 
@@ -31,4 +35,32 @@ button1.grid(row=0, column=0)
 button2.grid(row=1, column=0)
 button3.grid(row=2, column=0)
 
+#configure the columns in mainWindow. Adding weight to columns will populate empty space in column 
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1) #grid_columnconfigure is the same as columnconfigure
+
+#config() sets options. In this case options are a sunken boarder for: 
+#leftFrame which is in row=1 column=1 and rightFrame in row=1 column=2 of mainWindow
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+
+#grid(sticky= ) is used here to stretch the sunken boarder
+#using the compass references points: 'ns', 'new' 
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='new')
+
+#NOTE ! weight options must be set inside of a cell first in some cases 
+#as some options like sticky may not work.
+
+#setting weight options for rightFrame using columnconfigure   
+rightFrame.columnconfigure(0, weight=1)
+
+#now weight as been set above on line 53 we can use grid(sticky='ew') to stretch buttons
+button2.grid(sticky='ew') 
+
+
 mainWindow.mainloop()
+
+
+

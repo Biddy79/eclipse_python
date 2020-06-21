2c8a6b972e4224d509a4dba7ca7d329fcff168f5
Adam <a.beardsley79@gmail.com>
2020-06-21 20:13:06 +0100

    calculator_challenge: added all buttons

--- Tkinter/calculator_challenge/__init__.py
+++ Tkinter/calculator_challenge/__init__.py
@@ -30,30 +30,79 @@ except ImportError:
     
 mainWindow = tkinter.Tk()
 
-mainWindow.geometry('500x600-1200-400')
+mainWindow.geometry('300x375-1200-400')
 mainWindow.title("Calculator")
 
-mainWindow.rowconfigure(0, weight=10)
+
+mainWindow.rowconfigure(0, weight=0)
 mainWindow.rowconfigure(1, weight=0)
-mainWindow.rowconfigure(2, weight=500)
+mainWindow.rowconfigure(2, weight=0)
+mainWindow.rowconfigure(3, weight=0)
+mainWindow.rowconfigure(4, weight=0)
+mainWindow.rowconfigure(5, weight=0)
+mainWindow.rowconfigure(6, weight=100)
 
 mainWindow.columnconfigure(0, weight=0)
 mainWindow.columnconfigure(1, weight=0)
-mainWindow.columnconfigure(2, weight=500)
-mainWindow.columnconfigure(3, weight=500)
+mainWindow.columnconfigure(2, weight=0)
+mainWindow.columnconfigure(3, weight=0)
+mainWindow.columnconfigure(4, weight=100)
 
 
 resultBox = tkinter.Entry(mainWindow)
-resultBox.grid(row=0, column=0, sticky='nsew', columnspan=3)
+resultBox.grid(row=0, column=0, sticky='ew', columnspan=4)
 
 C_Button = tkinter.Button(mainWindow, text='C')
-C_Button.grid(row=1, column=0, sticky='w')
+C_Button.grid(row=1, column=0)
 
 CE_Button = tkinter.Button(mainWindow, text='CE')
-CE_Button.grid(row=1, column=1, sticky='w')
+CE_Button.grid(row=1, column=1, sticky='ew')
+
+Button_7 = tkinter.Button(mainWindow, text='7')
+Button_7.grid(row=2, column=0)
+
+Button_8 = tkinter.Button(mainWindow, text='8')
+Button_8.grid(row=2, column=1, sticky='ew')
+
+Button_9 = tkinter.Button(mainWindow, text='9')
+Button_9.grid(row=2, column=2)
+
+Button_plus = tkinter.Button(mainWindow, text='+')
+Button_plus.grid(row=2, column=3, sticky='ew')
+
+Button_4 = tkinter.Button(mainWindow, text='4')
+Button_4.grid(row=3, column=0)
+
+Button_5 = tkinter.Button(mainWindow,text='5')
+Button_5.grid(row=3, column=1, sticky='ew')
+
+Button_6 = tkinter.Button(mainWindow, text='6')
+Button_6.grid(row=3, column=2)
+
+Button_sub = tkinter.Button(mainWindow, text='-')
+Button_sub.grid(row=3, column=3, sticky='ew')
+
+Button_1 = tkinter.Button(mainWindow, text='1')
+Button_1.grid(row=4, column=0)
+
+Button_2 = tkinter.Button(mainWindow, text='2')
+Button_2.grid(row=4, column=1, sticky='ew')
+
+Button_3 = tkinter.Button(mainWindow, text='3')
+Button_3.grid(row=4, column=2)
+
+Button_mult = tkinter.Button(mainWindow, text='*')
+Button_mult.grid(row=4, column=3, sticky='ew')
+
+Button_0 = tkinter.Button(mainWindow, text='0')
+Button_0.grid(row=5, column=0)
+
+Button_equal = tkinter.Button(mainWindow, text='=')
+Button_equal.grid(row=5, column=1, sticky='ew', columnspan=2)
 
 
-
+Button_slash = tkinter.Button(mainWindow, text='/')
+Button_slash.grid(row=5, column=3, sticky='ew')
 
 
 mainWindow.mainloop()

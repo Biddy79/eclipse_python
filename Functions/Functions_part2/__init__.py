e83dbf1077e87ad139c0b7cad7ef25af33a61672
Adam Beardsley <a.beardsley79@gmail.com>
2020-07-21 12:22:48 +0100

    added more parameters to center_text funnction in Funaction_part2. Giving center_text same signature as the standerd print function

--- Functions/Functions_part2/__init__.py
+++ Functions/Functions_part2/__init__.py
@@ -1,8 +1,16 @@
-#modifying center_text function to take more than one argument
-
-def center_text(text):
-    text = str(text)#text is cast too a string in case a number is passed in as argument
+#modifying center_text function to take more than one argument using *
+#we can aslo set parameter values inside fuction brackets() using =
+#also adding parameters found in print function 
+#two function that have the same parameters are said to have the same signatuer
+def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep #adding sep parameter to end of text
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    #setting print parameters = to center_text function arguments
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    
     
 center_text("Hello world")
+center_text("I ", "Am ", "learing " , "python")
+center_text(1, 2, 3, sep=":")# setting sep parameter as : 

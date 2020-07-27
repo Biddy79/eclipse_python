32a3e63f3350f3e6c68650cbdc97c80d3158c73e
Adam Beardsley <a.beardsley79@gmail.com>
2020-07-27 12:24:15 +0100

    Parabola_function: added canvas2 obj, also used repr() and locals() functions to display objects and variabel types

--- Functions/Parabola_function/__init__.py
+++ Functions/Parabola_function/__init__.py
@@ -8,14 +8,16 @@ def parabola(x):
     y = x*x / 100
     return y
     
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() /2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin ))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
-    #canvas.create_line(0,0,200,700, fill="green")
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() /2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin ))
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
+    #locals() function returns all variables that have been defined with ina function
+    #Note locals() must be used within a function
+    print(locals())
     
 def plot(canvas, x ,y):
     canvas.create_line(x, y, x+1, y+1, fill="red")
@@ -23,12 +25,19 @@ def plot(canvas, x ,y):
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Parabola")
-mainWindow.geometry("940x680-1200-650")
+mainWindow.geometry("840x680-1200-650")
 
-canvas = tkinter.Canvas(mainWindow, width=940, height= 680)
+canvas = tkinter.Canvas(mainWindow, width=420, height = 680)
 canvas.grid(row=0, column=0)
 
+canvas2 = tkinter.Canvas(mainWindow, width=420, height = 680, background="blue")
+canvas2.grid(row=0, column=1)
+
+#repr() function prints object name ant type
+print(repr(canvas), repr(canvas2))
+
 draw_axes(canvas)
+draw_axes(canvas2)
 
 for x in range(-100, 100):
     y = parabola(x)

bf4e6e3ffed8cd6e72144986bc32045cc12af99d
Adam Beardsley <a.beardsley79@gmail.com>
2020-09-02 18:53:21 +0100

    Blackjack: added deal function this returns the next card which is poped from the deck and displays this in the frame. set this to work when dealer_button and player_button is selected by adding command as argument. also added deal_player and deal dealer function so dealer_card_frame and player_card_frame arguments can be added respective to where the card image is displayed.

--- Blackjack/blackjack/__init__.py
+++ Blackjack/blackjack/__init__.py
@@ -30,6 +30,23 @@ def load_images(card_images):
             name = 'C:\\Users\\Adam\\src\\eclipse_python\\Blackjack\\blackjack\\cards\\{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
             card_images.append((10, image,))
+
+
+def deal_card(frame):
+    #pop the next card off the top of the deck
+    next_card = deck.pop()
+    #add the image to the label and display the the label
+    tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
+    #now return the card's face value
+    return next_card
+    
+    
+def deal_dealer():
+    deal_card(dealer_card_frame)
+    
+    
+def deal_player():
+    deal_card(player_card_frame)
     
     
 #set up screen and frames for dealer nad player
@@ -60,10 +77,10 @@ player_card_frame.grid(row=2, column=1 , sticky='ew', rowspan=2)
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Player")
+player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
 player_button.grid(row=0, column=1)
 
 

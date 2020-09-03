debc15d4a0051bf74f0c9e532cb24c2b5925273c
Adam Beardsley <a.beardsley79@gmail.com>
2020-09-03 18:18:54 +0100

    Blackjack: mainWindow background configured to green, player_score and player_ace global veriabel added. player_score then set with in deal_player function and player_ace value tested to see if player as already drawn an ace if not card value will be assined 11. Value of card_value draw from deck is then added to player_score. result_text is then set to display message Dealer Wins! if player_score is grater than 21.

--- Blackjack/blackjack/__init__.py
+++ Blackjack/blackjack/__init__.py
@@ -5,7 +5,7 @@ try:
 except ImportError: #python 2
     import Tkinter as tkinter
     
-mainWindow = tkinter.Tk()
+
 
 def load_images(card_images):
     suits=['club', 'diamond', 'heart', 'spade']
@@ -34,7 +34,7 @@ def load_images(card_images):
 
 def deal_card(frame):
     #pop the next card off the top of the deck
-    next_card = deck.pop()
+    next_card = deck.pop(0)
     #add the image to the label and display the the label
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     #now return the card's face value
@@ -46,12 +46,26 @@ def deal_dealer():
     
     
 def deal_player():
-    deal_card(player_card_frame)
+    player_score = 0
+    card_value = deal_card(player_card_frame)[0]
+    if card_value == 1 and not player_ace:  
+        card_value = 11
+    player_score += card_value
+    #if bust check for ace and subtract 1
+    if player_score > 21 and player_ace:
+        player_score -= 10
+    player_score_label.set(player_score)
+    if(player_score > 21):
+        result_text.set("Dealer Wins!")
+    print(locals())
     
+    
+mainWindow = tkinter.Tk()
     
 #set up screen and frames for dealer nad player
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
+mainWindow.configure(background='green')
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
@@ -68,6 +82,8 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
+player_score = 0
+player_ace = False
 tkinter.Label(card_frame, text="player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 #embeded frame to hold the card images 

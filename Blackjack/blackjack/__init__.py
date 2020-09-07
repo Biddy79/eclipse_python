3ff21fc629c543a2be5d38c5f0d427c38a27c845
Adam Beardsley <a.beardsley79@gmail.com>
2020-09-07 18:13:19 +0100

    Blackjack: made changes to deal_player function, First by changing global player_score and global player_ace to update player_score each time a card is drawn also tested to see if player as drawn ace (if over 21, player is bust therfor only one ace can be drawn in a game).Then commented this code out a re-writen deal_player function which uses score_hand function to calculate player_score

--- Blackjack/blackjack/__init__.py
+++ Blackjack/blackjack/__init__.py
@@ -1,5 +1,6 @@
 import random
 
+
 try:
     import tkinter
 except ImportError: #python 2
@@ -40,24 +41,51 @@ def deal_card(frame):
     #now return the card's face value
     return next_card
     
+def score_hand(hand):
+    #calculate the total score of all cards in the list
+    #Only one ace can have a value 11, and this will be reduced to 1 if the hand would bust.
+    score = 0
+    ace = False
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+        score += card_value
+        #If bust, check if there is an ace and subtract 10
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
+    
     
 def deal_dealer():
     deal_card(dealer_card_frame)
     
     
 def deal_player():
-    player_score = 0
-    card_value = deal_card(player_card_frame)[0]
-    if card_value == 1 and not player_ace:  
-        card_value = 11
-    player_score += card_value
-    #if bust check for ace and subtract 1
-    if player_score > 21 and player_ace:
-        player_score -= 10
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
+    
     player_score_label.set(player_score)
-    if(player_score > 21):
+    if player_score > 21:
         result_text.set("Dealer Wins!")
-    print(locals())
+        
+#     global player_score
+#     global player_ace
+#     card_value = deal_card(player_card_frame)[0]
+#     if card_value == 1 and not player_ace:  
+#         player_ace = True
+#         card_value = 11
+#     player_score += card_value
+#     #if bust check for ace and subtract 1
+#     if player_score > 21 and player_ace:
+#         player_score -= 10
+#         player_ace = False
+#     player_score_label.set(player_score)
+#     if(player_score > 21):
+#         result_text.set("Dealer Wins!")
+#     print(locals())
     
     
 mainWindow = tkinter.Tk()
@@ -82,8 +110,7 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
-player_score = 0
-player_ace = False
+
 tkinter.Label(card_frame, text="player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 #embeded frame to hold the card images 

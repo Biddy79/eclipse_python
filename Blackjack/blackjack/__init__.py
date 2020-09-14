381bfb8fcd5fb6f460467a44e7c8923ef50f4aef
Adam Beardsley <a.beardsley79@gmail.com>
2020-09-14 13:59:44 +0100

    Blackjack: completed deal_dealer() function which sets dealer_score and adds logic to determin winner of game or if game is a draw

--- Blackjack/blackjack/__init__.py
+++ Blackjack/blackjack/__init__.py
@@ -60,7 +60,22 @@ def score_hand(hand):
     
     
 def deal_dealer():
-    deal_card(dealer_card_frame)
+    dealer_score = score_hand(dealer_hand)
+    while 0 < dealer_score < 17:
+        dealer_hand.append(deal_card(dealer_card_frame))
+        dealer_score = score_hand(dealer_hand)
+        dealer_score_label.set(dealer_score)
+    
+    player_score = score_hand(player_hand)
+    if player_score > 21:
+        result_text.set("Dealer wins!!")
+    elif dealer_score > 21 or dealer_score < player_score:
+        result_text.set("Player wins!!")
+    elif dealer_score > player_score:
+        result_text.set("Dealer wins!!")
+    else:
+        result_text.set("Draw!")
+        
     
     
 def deal_player():
@@ -141,6 +156,10 @@ random.shuffle(deck)
 dealer_hand = []
 player_hand = []
 
+deal_player()
+dealer_hand.append(deal_card(dealer_card_frame))
+dealer_score_label.set(score_hand(dealer_hand))
+deal_player()
 
 mainWindow.mainloop()
 

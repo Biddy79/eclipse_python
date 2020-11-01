f4f05c0193fdde1ceb108fa7d05f066708e74931
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-01 16:54:31 +0000

    Fibonacci_and_recursion: Added fibonacci function that dose NOT call itself

--- Namespace_scope_and_recursion/Fibonacci_and_recursion/__init__.py
+++ Namespace_scope_and_recursion/Fibonacci_and_recursion/__init__.py
@@ -21,15 +21,33 @@ def factorial(n):
    
 for i in range (130):
     print(i, factorial(i))
-    
+  
+#Fibonacci function that call itself    
 def fib(n):
     """ F(n) = F(n - 1) + F(n -2) """
     if n <2:
         return n 
     else:
         return fib(n-1) + fib(n-2)
+
+#Fibonacci function that dose NOT call itself   
+def fibonacci(n):
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minus1 = 1
+        n_minus2 = 0
+        for f in range(1, n):
+            result = n_minus2 + n_minus1
+            n_minus2 = n_minus1
+            n_minus1 = result
+    return result
+
+        
     
 print("=" * 30)
 
 for i in range(36):
-    print(i, fib(i))
+    print(i, fibonacci(i))

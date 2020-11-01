a2100d440e6edd0838c11dc0e478ab9649cb1d92
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-01 17:08:50 +0000

    File_and_Directories_recursive_function: Started project to writing a finction to list director and files using a recurrsive function

---
+++ Namespace_scope_and_recursion/File_and_Directories_recursive_function/__init__.py
@@ -0,0 +1,9 @@
+import os 
+
+listing = os.walk('.')
+for root , directories, files in listing:
+    print(root)
+    for d in directories:
+        print(d)
+    for f in files:
+        print(files)

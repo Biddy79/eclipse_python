fb55bb8a2e267676913da27d3d0a28513261bda3
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-02 14:33:56 +0000

    File_and_Directories_recursive_function: Defined a recursive function to list directories and files. Also used a nonlocal tab_stop veriabel to tab file list

--- Namespace_scope_and_recursion/File_and_Directories_recursive_function/__init__.py
+++ Namespace_scope_and_recursion/File_and_Directories_recursive_function/__init__.py
@@ -1,9 +1,28 @@
 import os 
 
-listing = os.walk('.')
-for root , directories, files in listing:
-    print(root)
-    for d in directories:
-        print(d)
-    for f in files:
-        print(files)
+
+def list_directories(s):
+    
+    def dir_list(d):
+        nonlocal tab_stop # nonlocal looks for veriabel within the encloseing scope. not defined!!
+        files = os.listdir(d)
+        for f in files:
+            current_dir = os.path.join(d, f)
+            if os.path.isdir(current_dir):
+                print("\t" * tab_stop + "Directory " + f)
+                tab_stop += 1
+                dir_list(current_dir)
+                tab_stop -= 1
+            else:
+                print("\t" * tab_stop + f)
+    
+    
+    tab_stop = 0 # tab_stop veriabel is within the encloseing scope of list_directories function 
+    if os.path.exists(s):
+        print('directory listing of ' + s)
+        dir_list(s)
+    else:
+        print(s+ " Does not exist")
+    
+
+list_directories('.')

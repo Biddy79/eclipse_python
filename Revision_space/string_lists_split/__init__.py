11e10827d9d363f997d6f5d0b44aed05f5b25d02
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-04 16:17:20 +0000

    Revision_space: Place to practice what I have learnt

---
+++ Revision_space/string_lists_split/__init__.py
@@ -0,0 +1,12 @@
+numbers = "102938 0988"
+
+number = numbers[0]
+print(number)
+
+my_list = ["I like, Winter".split(',')]
+print(my_list[0])
+
+user_input = input("Enter: ")
+print(user_input.split()[0])
+
+

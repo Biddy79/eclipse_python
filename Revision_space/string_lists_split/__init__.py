ee309b29f5f6c4cb7ecdff60679dcb721b46953c
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-04 17:18:02 +0000

    Revision_space: updated

--- Revision_space/string_lists_split/__init__.py
+++ Revision_space/string_lists_split/__init__.py
@@ -3,10 +3,17 @@ numbers = "102938 0988"
 number = numbers[0]
 print(number)
 
-my_list = ["I like, Winter".split(',')]
-print(my_list[0])
+#when storing a string in a variabel you can index each char with array brackets []
+my_list = "I like winter"
+print(my_list[2])
 
-user_input = input("Enter: ")
-print(user_input.split()[0])
+my_list1 = "I,like,winter"
+#split() will remove what ever char is passed as parameter. No parameter will defalt to a space
+#Here we have split the string by ',' This removes all commas and separates each word between them
+#into its own index which can then be selected using []
+print(my_list1.split(',')[1])
 
+my_list2 = ["I like Winter".split()]
+print(my_list2[0][2])
+print(len(my_list2))
 

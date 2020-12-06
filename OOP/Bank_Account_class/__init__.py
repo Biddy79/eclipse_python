32832ca50ac1ff17e9b2d8d9bdbd22db6e9ec3af
Adam Beardsley <a.beardsley79@gmail.com>
2020-12-06 16:40:34 +0000

    OPP Banck_Account_class: transaction_list attribute to store tupel of date, time and amount in transcations line 18
    static method declared _current_time this returns local time lins 10-13
    
    transaction_list appended to each time a call is made to deposit or withdraw function line 29 and 34
    a tuple is added which stores current time, date (by making a call to _current_time function) and amount
    
    lines 43-49 show_transactions loops through date and amount in transaction_list setting tran_type to either deposited or withdraw
    line 50 then prints amount tran_type date/time and local date/time
    
    line 64 makes call to show_transaction

--- OOP/Bank_Account_class/__init__.py
+++ OOP/Bank_Account_class/__init__.py
@@ -1,9 +1,21 @@
+import datetime
+import pytz
+from _datetime import date
+
+
 class Account():
     '''simple account class with balance'''
+    
+    #staticmethods are called using class name call made on lines 29 and 34
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
     
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print(f"Account created for {self.name}")
         
     def deposit(self, amount):
@@ -11,17 +23,32 @@ class Account():
             self.balance += amount
             #call to method from within a method using self!!!!
             self.show_balance()
-        
+            #appending transactions to transaction_list
+            #self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+            #we can use static method to append datetime to transaction_list like below line 28
+            self.transaction_list.append((Account._current_time(), amount))
+            
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))#note - amount here!
         else:
             print("Amount must be greater than 0 and must not exceed your balance")
             #call to method from within a method using self!!!!
-            self.show_balance()
+        self.show_balance()
             
     def show_balance(self):
         print(f"Your balance is {self.balance}")
+        
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1 #to show negative number
+            print(f"{amount:6} {tran_type} on {date} local time was {date.astimezone()}")
+        
         
 if __name__ == '__main__':
     Adam = Account('Adam', 0)
@@ -34,3 +61,6 @@ if __name__ == '__main__':
     Adam.show_balance()
     
     Adam.withdraw(500)
+    Adam.show_transactions()
+    
+    

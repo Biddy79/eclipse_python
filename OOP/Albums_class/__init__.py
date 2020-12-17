8d653c20ae58d0a94f28d55fb3b502eeac269a1e
Adam Beardsley <a.beardsley79@gmail.com>
2020-12-17 14:47:42 +0000

    OOP Album_class: lines 132-135 set if statment outside of for loop and added and added (not) condition to statmens allowing for proper input of last album and artist objects
    lines 139-145 function that takes list of Artist and Album object and prints relavent attribute data to a txt file
    (checkfile.txt) this file is then created and can be checked against the original albums.txt file
    
    line 149 set artist equal to return artist_list in load_data function
    
    line 151 prints lenght of artist_list
    
    line 153 call to create_checkfile function passing in artist list
    
    lines 156-163
    using arry syntax to acsess Artist and Album objects stored in artist list and printsing out there attributes
    
    added more comments overall to code

--- OOP/Albums_class/__init__.py
+++ OOP/Albums_class/__init__.py
@@ -114,22 +114,51 @@ def load_data():
                 new_album = None
             
             if new_album is None:
-                #create new Album object
+                #create new Album object if None
                 new_album = Album(album_field, year_field, new_artist)
+            #check to see if album name as changed
             elif new_album.name != album_field:
+                #add album object to album list in Artist class object
                 new_artist.add_album(new_album)
+                #new Album object instantiated with new filed args
                 new_album = Album(album_field, year_field, new_artist)
-                
-            if new_artist is None:
-                if new_album is None:
-                    new_artist.add_album(new_album)
-                artist_list.append(new_artist)
-                
+            #create instance of Song object populate with song and artist field args
+            new_song = Song(song_field, new_artist)
+            #add new_song object to trak list in new_album object class
+            new_album.add_song(new_song)    
+            
+        #After last line in .txt file we will have Artist and Album object that as not been stored 
+        #therfor we will add them here! 
+        if new_artist is not None:
+            if new_album is not None:
+                new_artist.add_album(new_album)
+            artist_list.append(new_artist)
+    #return a list of Artist and Album objects       
     return artist_list
-                
-                
+
+def create_checkfile(artist_list):                
+    """Create a check file from object data for comparison with the original file"""
+    with open("checkfile.txt", 'w') as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.album:
+                for new_song in new_album.track:
+                    print(f"{new_artist.name}\t{new_album.name}\t{new_album.year}\t{new_song.titel}", file=checkfile)
+    
+    
 if __name__ == "__main__":
     artists = load_data()
+    
     print(f"There are {len(artists)} artists")
     
+    create_checkfile(artists)
+
+    
+    #prints First artist name in .txt file
+    print(artists[0].name)
+    #prints forth artists third album name in .txt file
+    print(artists[3].album[2].name)
+    #prints third artists first album, second track name in .txt
+    print(artists[2].album[0].track[1].titel)
+    #prints third artist, first album second track artist(obj).name
+    print(artists[2].album[0].track[1].artist.name)
     

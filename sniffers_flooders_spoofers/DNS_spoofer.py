dbd8308c459a90c16383f0458cdc0a0e42c39a5f
Adam Beardsley <a.beardsley79@gmail.com>
2021-09-20 19:09:58 +0100

    DNS_spoofer.py removed basic statment to print out dns request information. Filltering packets form iptables rule number 0. Passing packets to process_packet finction where payload from packet is got.

--- sniffers_flooders_spoofers/DNS_spoofer.py
+++ sniffers_flooders_spoofers/DNS_spoofer.py
@@ -3,12 +3,30 @@ Created on 27 Aug 2021
 
 @author: adam
 '''
-#!/usr/bin/python3
+#!/usr/bin/python   
+
+#May not work in python 3 may need to use python 2
+#!/usr/bin/env python2.7
 
 from scapy.all import * 
+from netfilterqueue import NetfilterQueue
 
-def find_DNS(p):
-    if p.haslayer(DNS):
-        print(p[IP].src, p[DNS].summary())
+def proces_packet(packet):
+    pkt = IP(packet.get_payload()) #converts the raw packet to a scapy compatible string
 
-sniff(prn=find_DNS)
+    #modify the packet here
+
+    packet.set_payload(str(pkt)) #set the packet content to our modified version
+
+    packet.accept() #accept the packet
+    
+    
+
+nfqueue = NetfilterQueue()
+#0 is the iptabels rule queue number, modify is the callback function
+nfqueue.bind(0, proces_packet) 
+
+try:
+    print "[*] waiting for data"
+    nfqueue.run()
+except KeyboardInterrupt:

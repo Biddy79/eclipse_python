91565241fbff68cbab2fb76376b06fb07a8e58fa
Adam Beardsley <a.beardsley79@gmail.com>
2021-10-19 15:06:28 +0100

    Network_Analysis: socket_sniffer.py: Completed analyze_ether_header function which formats and prints out ether header returning remining data and ip protocol. analyze_ip_header function to be completed. This will take remaning data to format and print ip header when completed.

--- Network_Analysis/socket_sniffer.py
+++ Network_Analysis/socket_sniffer.py
@@ -22,7 +22,19 @@ def analyze_ether_header(data_recv):
     proto = eth_hdr[2] >> 8
     data = data_recv[14:]
     
+    print("____________ETHERNET HEADER____________")
+    print(f"Destination MAC: {dst_mac[0:2]}:{dst_mac[2:4]}:{dst_mac[4:6]}:{dst_mac[6:8]}:{dst_mac[8:10]}:{dst_mac[10:12]}")
+    print(f"Source MAC: {src_mac[0:2]}:{src_mac[2:4]}:{src_mac[4:6]}:{src_mac[6:8]}:{src_mac[8:10]}:{src_mac[10:12]}")
+    print(f"Protocol: {proto}")
+    
+    if proto == 0x08:
+        ip_bool = True
+        
+    return data, ip_bool
 
+def analyze_ip_header(data_recv):
+    ip_hrd = struct.unpack('!6H4s4s', data_recv[:20])
+    
 
 def main():
     if sock_created == False:
@@ -32,4 +44,19 @@ def main():
     data_recv = sniffer_socket.recv(2048)
     os.system('clear')
     
-    formated_data_recv, ip_bool = analyze_ether_header(data_recv)
+    data_recv, ip_bool = analyze_ether_header(data_recv)
+    
+    if ip_bool:
+        data_recv, tcp_udp = analyze_ip_header(data_recv)
+    else:
+        return
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
